f0f73ebf2f03d7c8789620e3b2d61954ecb9343b
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-06-21 09:16:01 +0530

    Refactor: Update server response logic and enhance HTML structure

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const fs = require("fs");
 const http = require("http");
 const html = fs.readFileSync("./index.html", "utf-8");
 const server = http.createServer((request, response) => {
-  response.end(html);
-  console.log("New request received from browser");
+  let path = request.url;
+  if (path === "/" || path.toLocaleLowerCase() === "/home") {
+    response.end(html.replace("{{%CONTENT%}}", "You are in Home page"));
+  } else if (path.toLocaleLowerCase() === "/about") {
+    response.end(html.replace("{{%CONTENT%}}", "You are in About page"));
+  } else {
+    response.end(html.replace("{{%CONTENT%}}", "Page not found"));
+  }
 });
 
 server.listen(4000, "127.0.0.1", () => {
