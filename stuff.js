fc64f78d8ed5070e4e74b2a10ee2c062ab2cb4b5
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-06-29 11:40:58 +0530

    node event emiiter

--- stuff.js
+++ stuff.js
@@ -1,5 +1,23 @@
-var stuff = require("./app.js");
+var events = require("events");
+var util = require("util");
 
-console.log(stuff.counter(["Shawn", "Paul", "John", "George", "Ringo"]));
-console.log(stuff.adder(5, 10));
-console.log(stuff.pi);
+var Person = function (name) {
+  this.name = name;
+};
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person("James");
+var mary = new Person("Mary");
+var john = new Person("John");
+
+var people = [james, mary, john];
+
+people.forEach(function (person) {
+  person.on("speak", function (msg) {
+    console.log(person.name + " said: " + msg);
+  });
+});
+
+people.forEach(function (person) {
+  person.emit("speak", "Hello, everyone!");
+});
