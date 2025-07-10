df981ada948a2c57bc74c504e673c5d0bf4785f4
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-07-10 15:13:32 +0530

    Add initial views for contact, index, navigation, and profile pages
    - Created contact.ejs with a basic structure and navigation inclusion.
    - Developed index.ejs with styling and a welcome message.
    - Implemented nav.ejs for site navigation links.
    - Established profile.ejs to display user information and hobbies with styling.

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var http = require("http");
-var fs = require("fs");
+var express = require("express");
+var app = express();
 
-var server = http.createServer(function (req, res) {
-  console.log("request was made: " + req.url);
-  res.writeHead(200, { "content-type": "text/html" });
-  var myObj = {
-    name: "John Doe",
-    age: 30,
-    occupation: "Software Developer",
-  };
-  res.end(JSON.stringify(myObj)); //we have to convert the object to a string before sending it
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  console.log("Request was made: " + req.url);
+  res.render("partials/index");
 });
 
-server.listen(3000, "127.0.0.1");
-console.log("Server is listening on port 3000");
+app.get("/contact", function (req, res) {
+  console.log("Request was made: " + req.url);
+  res.render("partials/contact");
+});
+
+app.get("/profile/:name", function (req, res) {
+  var data = { age: 29, job: "ninja" };
+  res.render("profile", {
+    person: req.params.name,
+    data: data,
+    hobbies: ["eating", "sleeping", "coding"],
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on http://localhost:3000");
+});
