d49f4027dba998aba4878d21361350908ec2230b
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-07-11 16:40:50 +0530

    Refactor: Enhance contact form handling and add success page; integrate body-parser and static assets

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
-app.set("view engine", "ejs");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.set("view engine", "ejs");
+app.use("/assets", express.static("assets"));
 app.get("/", function (req, res) {
   console.log("Request was made: " + req.url);
   res.render("partials/index");
 });
 
 app.get("/contact", function (req, res) {
-  console.log("Request was made: " + req.url);
-  res.render("partials/contact");
+  res.render("partials/contact", { qs: req.query });
+});
+
+app.post("/contact", urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.render("partials/contact-success", { qs: req.body });
 });
 
 app.get("/profile/:name", function (req, res) {
   var data = { age: 29, job: "ninja" };
-  res.render("profile", {
+  res.render("partials/profile", {
     person: req.params.name,
     data: data,
     hobbies: ["eating", "sleeping", "coding"],
