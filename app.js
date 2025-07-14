831f43e1f5f6fc7b271d17c1009230711ec20221
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-07-15 02:47:35 +0530

    enter to the crash course

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+// const name = "janada";
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// console.log(name);
 
-app.set("view engine", "ejs");
-app.use("/assets", express.static("assets"));
-app.get("/", function (req, res) {
-  console.log("Request was made: " + req.url);
-  res.render("partials/index");
-});
+const greet = (name) => {
+  console.log(`Hello, ${name}!`);
+};
 
-app.get("/contact", function (req, res) {
-  res.render("partials/contact", { qs: req.query });
-});
-
-app.post("/contact", urlencodedParser, function (req, res) {
-  console.log(req.body);
-  res.render("partials/contact-success", { qs: req.body });
-});
-
-app.get("/profile/:name", function (req, res) {
-  var data = { age: 29, job: "ninja" };
-  res.render("partials/profile", {
-    person: req.params.name,
-    data: data,
-    hobbies: ["eating", "sleeping", "coding"],
-  });
-});
-
-app.listen(3000, function () {
-  console.log("Server is running on http://localhost:3000");
-});
+greet("janada");
