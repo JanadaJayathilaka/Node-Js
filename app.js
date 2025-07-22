6a116f03ae5170e92da473d75d5e0ddf635680aa
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-07-22 20:06:00 +0530

    feat: enhance blog functionality; add create blog page, update index and about pages, and improve 404 handling

--- app.js
+++ app.js
@@ -13,17 +13,35 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   // res.send("<h1>Home Page</h1>");
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home Page", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About page" });
 });
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Page" });
+});
+
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "error page" });
 });
