9ff810615d083f6244c18eeccecaa734e243d153
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-07-25 13:16:44 +0530

    post get params

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { result } = require("lodash");
+const { urlencoded } = require("body-parser");
 
 //express app setup
 const app = express();
@@ -8,42 +11,23 @@ const app = express();
 //connect mongodb
 const dbURI = "mongodb://127.0.0.1:27017/netninja";
 
-mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Connected to DB and listening on port 3000");
-    });
+mongoose.connect(dbURI).then(() => {
+  app.listen(3000, () => {
+    console.log("Connected to DB and listening on port 3000");
   });
+});
 
 //register view engine
 app.set("view engine", "ejs");
 
 //middleware and static files
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   // res.send("<h1>Home Page</h1>");
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home Page", blogs: blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
@@ -52,6 +36,42 @@ app.get("/about", (req, res) => {
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then(() => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).render("404", { title: "Blog not found" });
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
